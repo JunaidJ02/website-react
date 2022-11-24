eaca7f17af8dadb1786da61e378d20c1da936721
junaidjaved <JunaidJ.2002@gmail.com>
2022-11-24 16:39:41 +0300

    Updates

---
+++ precache-manifest.35706c4ec99d6bd4ec252a2411804b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b50ed71bd350372b803",
+    "url": "/static/css/main.9df058d9.chunk.css"
+  },
+  {
+    "revision": "0b50ed71bd350372b803",
+    "url": "/static/js/main.0b50ed71.chunk.js"
+  },
+  {
+    "revision": "5a63e80967d755f43234",
+    "url": "/static/js/1.5a63e809.chunk.js"
+  },
+  {
+    "revision": "99a9dfcd4beecc161e1b",
+    "url": "/static/js/2.99a9dfcd.chunk.js"
+  },
+  {
+    "revision": "55222b96e37d19236616",
+    "url": "/static/js/runtime~main.55222b96.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f553f9f65f02aee19ff28512d166a66e",
+    "url": "/index.html"
+  }
+];
